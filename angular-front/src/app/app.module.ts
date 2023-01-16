14113598c87abc6f6f329e9c0d160b13a8b428ff
alexis.baladon <alexis.baladon@fing.edu.uy>
2023-01-16 15:00:14 -0300

    ng-components-music

--- angular-front/src/app/app.module.ts
+++ angular-front/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { TableComponent } from './components/table/table.component';
+import { CardsComponent } from './components/cards/cards.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    MenuComponent,
+    DashboardComponent,
+    TableComponent,
+    CardsComponent
   ],
   imports: [
     BrowserModule
