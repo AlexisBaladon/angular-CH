f8e301a800e661b02c3eea2176501684071cab22
alexis.baladon <alexis.baladon@fing.edu.uy>
2023-03-14 13:10:10 -0300

    change in origins

--- mean-back/src/config/allowedOrigins.ts
+++ mean-back/src/config/allowedOrigins.ts
@@ -1,6 +1,6 @@
 const allowedOrigins = [
     'http://localhost:4200', // TODO change to deploy domain
-    'https://angular-coderhouse-alexis-baladon.netlify.app/'
+    'https://angular-coderhouse-alexis-baladon.netlify.app'
 ]
 
 export {allowedOrigins}
