964005952bd22d3cdbaa0e4f36611a5d41401602
alexis.baladon <alexis.baladon@fing.edu.uy>
2023-03-14 13:28:51 -0300

    deployed origin added

--- mean-back/src/middlewares/cors.middleware.ts
+++ mean-back/src/middlewares/cors.middleware.ts
@@ -2,6 +2,7 @@ import {allowedOrigins} from "../config/allowedOrigins";
 
 const corsCredentials = (req, res, next) => {
     const origin = req.headers.origin;
+    console.log(origin)
     if (origin && allowedOrigins.includes(origin)) {
         res.header('Access-Control-Allow-Credentials', 'true');
     }
